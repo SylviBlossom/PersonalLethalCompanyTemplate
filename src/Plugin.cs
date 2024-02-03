1a05bed852c426c8495379a2adc6cff74b3fe893
SylviBlossom <sylviblossom@gmail.com>
2024-02-02 20:36:13 -0700

    Use tabs instead of spaces, fix inconsistent spacing in csproj

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -5,9 +5,9 @@ namespace TemplateMod;
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    private void Awake()
-    {
-        // Plugin startup logic
-        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-    }
+	private void Awake()
+	{
+		// Plugin startup logic
+		Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+	}
 }
--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -2,7 +2,7 @@ namespace TemplateMod;
 
 public static class PluginInfo
 {
-    public const string PLUGIN_GUID = "TemplateMod";
-    public const string PLUGIN_NAME = "TemplateMod";
-    public const string PLUGIN_VERSION = "1.0.0";
+	public const string PLUGIN_GUID = "TemplateMod";
+	public const string PLUGIN_NAME = "TemplateMod";
+	public const string PLUGIN_VERSION = "1.0.0";
 }
